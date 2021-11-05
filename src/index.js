cb368d8c7b660e537624f98dc90287682884400d
lag2331 <lag2331@gmail.com>
2021-11-05 10:20:29 +0900

    remove

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   document.getElementById('root'),
 );
